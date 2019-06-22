ea5808a8f84c83f160a61d86ae88d6bbdd9b398b
Pietro <pietro@siliani.it>
2019-06-22 12:54:42 +0200

    Aggiunto Unit Test per costruttore di base (classe Knight)

---
+++ unit_test/KnightTest.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Pietro on 2019-06-22.
+//
+
+#include "gtest/gtest.h"
+#include "../Knight.h"
+
+TEST(KnightTest, DefaultConstructor) {
+    Knight k(5, 0);
+    EXPECT_EQ(5, k.getPosX());
+    EXPECT_EQ(0, k.getPosY());
+    EXPECT_EQ(false, k.isOnFire());
+    EXPECT_EQ(20., k.getHealth());
+}
+
+
+TEST(KnightTest, TrueOnFireTest) {
+    Knight k(-5, 0, true);
+    EXPECT_EQ(-5, k.getPosX());
+    EXPECT_EQ(0, k.getPosY());
+    EXPECT_EQ(true, k.isOnFire());
+    EXPECT_EQ(20., k.getHealth());
+
+}
+
+
+TEST(KnightTest, NotDefHealthTest) {
+    Knight k(0, -5, false, 540.);
+    EXPECT_EQ(0, k.getPosX());
+    EXPECT_EQ(-5, k.getPosY());
+    EXPECT_EQ(false, k.isOnFire());
+    EXPECT_EQ(540., k.getHealth());
+}
