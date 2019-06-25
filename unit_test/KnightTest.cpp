ab7d16c904c2601975e5b3b32e797ab1990b8c6b
Pietro <pietro@siliani.it>
2019-06-25 18:07:45 +0200

    Aggiornato unit test

--- unit_test/KnightTest.cpp
+++ unit_test/KnightTest.cpp
@@ -10,7 +10,7 @@
 
 //Knight(float x, float y, bool onf = false, float h = defHealth, bool facingR = true, float s = HERO_SPEED);
 
-TEST(KnightTest, DefaultConstructor) {
+TEST(KnightTest, Constructor) {
     Knight k(5, 0);
     EXPECT_EQ(5, k.getSprite().getPosition().x);
     EXPECT_EQ(0, k.getSprite().getPosition().y);
@@ -21,7 +21,7 @@ TEST(KnightTest, DefaultConstructor) {
 }
 
 
-TEST(KnightTest, isOnFire) {
+TEST(KnightTest, IsOnFire) {
     Knight k(-5, 0, true);
     EXPECT_EQ(-5, k.getSprite().getPosition().x);
     EXPECT_EQ(0, k.getSprite().getPosition().y);
@@ -44,7 +44,7 @@ TEST(KnightTest, NotDefHealth) {
 }
 
 
-TEST(KnightTest, isFacingLeft) {
+TEST(KnightTest, IsFacingLeft) {
     Knight k(0, -5, false, DEF_GAMEHERO_HEALTH, false);
     EXPECT_EQ(0, k.getSprite().getPosition().x);
     EXPECT_EQ(-5, k.getSprite().getPosition().y);
@@ -54,7 +54,7 @@ TEST(KnightTest, isFacingLeft) {
     EXPECT_EQ(HERO_SPEED, k.getMovementSpeed());
 }
 
-TEST(KnightTest, movementSpeed) {
+TEST(KnightTest, MovementSpeed) {
     Knight k(0, -5, false, DEF_GAMEHERO_HEALTH, false, 10.);
     EXPECT_EQ(0, k.getSprite().getPosition().x);
     EXPECT_EQ(-5, k.getSprite().getPosition().y);
@@ -62,4 +62,28 @@ TEST(KnightTest, movementSpeed) {
     EXPECT_EQ(DEF_GAMEHERO_HEALTH, k.getHealth());
     EXPECT_EQ(false, k.isFacingRight());
     EXPECT_EQ(10., k.getMovementSpeed());
+}
+
+TEST(KnightTest, Assigment) {
+    Knight k(-5, 3, true, 32.15, true, 5.);
+    Knight newK = k;
+    EXPECT_EQ(newK.getSprite().getPosition().x, k.getSprite().getPosition().x);
+    EXPECT_EQ(newK.getSprite().getPosition().y, k.getSprite().getPosition().y);
+    EXPECT_EQ(newK.isOnFire(), k.isOnFire());
+    EXPECT_EQ(newK.getHealth(), k.getHealth());
+    EXPECT_EQ(newK.isFacingRight(), k.isFacingRight());
+    EXPECT_EQ(newK.getMovementSpeed(), k.getMovementSpeed());
+
+}
+
+TEST(KnightTest, Copy) {
+    Knight k(5, 0, true, 25., false, 0.);
+    Knight copy(k);
+    EXPECT_EQ(copy.getSprite().getPosition().x, k.getSprite().getPosition().x);
+    EXPECT_EQ(copy.getSprite().getPosition().y, k.getSprite().getPosition().y);
+    EXPECT_EQ(copy.isOnFire(), k.isOnFire());
+    EXPECT_EQ(copy.getHealth(), k.getHealth());
+    EXPECT_EQ(copy.isFacingRight(), k.isFacingRight());
+    EXPECT_EQ(copy.getMovementSpeed(), k.getMovementSpeed());
+
 }
--- unit_test/WizardTest.cpp
+++ unit_test/WizardTest.cpp
@@ -9,7 +9,7 @@
 
 //Wizard(float x, float y, bool onf = false, float m = 10., float h = defHealth, bool facingR = true, float s = HERO_SPEED);
 
-TEST(WizardTest, DefaultConstructorTest) {
+TEST(WizardTest, Constructor) {
     Wizard w(1,1);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
     EXPECT_EQ(1, w.getSprite().getPosition().y);
@@ -18,7 +18,7 @@ TEST(WizardTest, DefaultConstructorTest) {
     EXPECT_EQ(10., w.getMana());
 }
 
-TEST(WizardTest, TrueOnFireTest) {
+TEST(WizardTest, TrueOnFire) {
     Wizard w(1,1, true);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
     EXPECT_EQ(1, w.getSprite().getPosition().y);
@@ -28,7 +28,7 @@ TEST(WizardTest, TrueOnFireTest) {
 }
 
 
-TEST(WizardTest, NotDefHealthTest) {
+TEST(WizardTest, NotDefHealth) {
     Wizard w(1,1, false, 10., 34.);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
@@ -38,7 +38,7 @@ TEST(WizardTest, NotDefHealthTest) {
 }
 
 
-TEST(WizardTest, NotDefManaTest) {
+TEST(WizardTest, NotDefMana) {
     Wizard w(1,1, false, 3., DEF_GAMEHERO_HEALTH);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
@@ -48,7 +48,7 @@ TEST(WizardTest, NotDefManaTest) {
 }
 
 
-TEST(WizardTest, isFacingLeft) {
+TEST(WizardTest, IsFacingLeft) {
     Wizard w(0, -5, false, 10., DEF_GAMEHERO_HEALTH, false);
     EXPECT_EQ(0, w.getSprite().getPosition().x);
     EXPECT_EQ(-5, w.getSprite().getPosition().y);
@@ -59,7 +59,7 @@ TEST(WizardTest, isFacingLeft) {
     EXPECT_EQ(HERO_SPEED, w.getMovementSpeed());
 }
 
-TEST(WizardTest, movementSpeed) {
+TEST(WizardTest, MovementSpeed) {
     Wizard w(1, -1, false, 10., DEF_GAMEHERO_HEALTH, true, 35.);
     EXPECT_EQ(1, w.getSprite().getPosition().x);
     EXPECT_EQ(-1, w.getSprite().getPosition().y);
@@ -68,4 +68,27 @@ TEST(WizardTest, movementSpeed) {
     EXPECT_EQ(10., w.getMana());
     EXPECT_EQ(true, w.isFacingRight());
     EXPECT_EQ(35., w.getMovementSpeed());
+}
+
+TEST(WizardTest, Assigment) {
+    Wizard w(1, 1, true, 10., 23., true, -90.);
+    Wizard newW = w;
+    EXPECT_EQ(newW.getSprite().getPosition().x, w.getSprite().getPosition().x);
+    EXPECT_EQ(newW.getSprite().getPosition().y, w.getSprite().getPosition().y);
+    EXPECT_EQ(newW.isOnFire(), w.isOnFire());
+    EXPECT_EQ(newW.getHealth(), w.getHealth());
+    EXPECT_EQ(newW.isFacingRight(), w.isFacingRight());
+    EXPECT_EQ(newW.getMovementSpeed(), w.getMovementSpeed());
+
+}
+
+TEST(WizardTest, Copy) {
+    Wizard w(-1, -1, false, 8., 10., false, 2.);
+    Wizard copy(w);
+    EXPECT_EQ(copy.getSprite().getPosition().x, w.getSprite().getPosition().x);
+    EXPECT_EQ(copy.getSprite().getPosition().y, w.getSprite().getPosition().y);
+    EXPECT_EQ(copy.isOnFire(), w.isOnFire());
+    EXPECT_EQ(copy.getHealth(), w.getHealth());
+    EXPECT_EQ(copy.isFacingRight(), w.isFacingRight());
+    EXPECT_EQ(copy.getMovementSpeed(), w.getMovementSpeed());
 }
