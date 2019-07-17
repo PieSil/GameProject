a328d5d914a8c9d7688013b5480b5fbb4503b342
Pietro <pietro@siliani.it>
2019-07-17 13:42:40 +0200

    Eliminata un'istruzione else superflua

--- Map.cpp
+++ Map.cpp
@@ -8,8 +8,8 @@
 /**
  * Map implementation
  */
-Map::Map(const int *tilesArray, const int &arrayColumns, const int &arrayRows, const std::string &path,
-         sf::Vector2u tileSize) : tilesArray(tilesArray), columns(arrayColumns), rows(arrayRows), tileSize(tileSize) {
+Map::Map(const int *tilesArray, const int &arrayColumns, const int &arrayRows, const std::string &path, sf::Vector2u tileSize)
+        : tilesArray(tilesArray), columns(arrayColumns), rows(arrayRows), tileSize(tileSize) {
     if (!tileset.loadFromFile(path)) {
         //TODO: throw exception
     }
@@ -33,34 +33,30 @@ void Map::load() {
             int tileNumber = tilesArray[j + i * columns]; // i + j*columns finds the ij element in the matrix
 
 
-                // find its position in the tileset texture
-                int tileTop = tileNumber % (tileset.getSize().x / tileSize.x);
-                int tileLeft = tileNumber / (tileset.getSize().x / tileSize.x);
+            // find its position in the tileset texture
+            int tileTop = tileNumber % (tileset.getSize().x / tileSize.x);
+            int tileLeft = tileNumber / (tileset.getSize().x / tileSize.x);
 
-                // get a pointer to the current tile's quad
-                sf::Vertex *quad = &vertices[(j + i * columns) * 4]; //multiply by 4 because each quad is made of 4 vertices
+            // get a pointer to the current tile's quad
+            sf::Vertex *quad = &vertices[(j + i * columns) * 4]; //multiply by 4 because each quad is made of 4 vertices
 
-                // define its 4 corners
-                quad[0].position = sf::Vector2f(j * tileSize.x, i * tileSize.y); //top left corner
-                quad[1].position = sf::Vector2f((j + 1) * tileSize.x, i * tileSize.y); //top right corner
-                quad[2].position = sf::Vector2f((j + 1) * tileSize.x, (i + 1) * tileSize.y); //bottom right corner
-                quad[3].position = sf::Vector2f(j * tileSize.x, (i + 1) * tileSize.y); //bottom left corner
+            // define its 4 corners
+            quad[0].position = sf::Vector2f(j * tileSize.x, i * tileSize.y); //top left corner
+            quad[1].position = sf::Vector2f((j + 1) * tileSize.x, i * tileSize.y); //top right corner
+            quad[2].position = sf::Vector2f((j + 1) * tileSize.x, (i + 1) * tileSize.y); //bottom right corner
+            quad[3].position = sf::Vector2f(j * tileSize.x, (i + 1) * tileSize.y); //bottom left corner
 
-                if (tileNumber != 0) {
-                    tiles.emplace_back(Tile(sf::Vector2f((quad[0].position.x + quad[1].position.x) / 2.,
-                                                         (quad[0].position.y + quad[3].position.y) / 2.), tileNumber));
-                    // define its 4 texture coordinates
-                    quad[0].texCoords = sf::Vector2f(tileTop * tileSize.x, tileLeft * tileSize.y); //top left corner
-                    quad[1].texCoords = sf::Vector2f((tileTop + 1) * tileSize.x, tileLeft * tileSize.y); //top right corner
-                    quad[2].texCoords = sf::Vector2f((tileTop + 1) * tileSize.x,
-                                                     (tileLeft + 1) * tileSize.y); //bottom right corner
-                    quad[3].texCoords = sf::Vector2f(tileTop * tileSize.x,
-                                                     (tileLeft + 1) * tileSize.y); //bottom left corner
+            tiles.emplace_back(Tile(sf::Vector2f((quad[0].position.x + quad[1].position.x) / 2.,
+                                                 (quad[0].position.y + quad[3].position.y) / 2.), tileNumber));
 
-                } else {
-                    tiles.emplace_back(Tile(sf::Vector2f((quad[0].position.x + quad[1].position.x) / 2.,
-                                                         (quad[0].position.y + quad[3].position.y) / 2.), tileNumber));
-                }
+            if (tileNumber != 0) {
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tileTop * tileSize.x, tileLeft * tileSize.y); //top left corner
+                quad[1].texCoords = sf::Vector2f((tileTop + 1) * tileSize.x, tileLeft * tileSize.y); //top right corner
+                quad[2].texCoords = sf::Vector2f((tileTop + 1) * tileSize.x, (tileLeft + 1) * tileSize.y); //bottom right corner
+                quad[3].texCoords = sf::Vector2f(tileTop * tileSize.x,
+                                                 (tileLeft + 1) * tileSize.y); //bottom left corner
+            }
         }
     }
 }
